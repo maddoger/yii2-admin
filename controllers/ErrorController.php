16b30798f02a3a17b23f37f4fc8a5c535c87f13b
Vitaliy Syrchikov <maddoger@gmail.com>
2014-04-11 22:05:37 +0400

    Exception to errorHandler

--- controllers/ErrorController.php
+++ controllers/ErrorController.php
@@ -34,7 +34,7 @@ class ErrorController extends Controller
 
 	public function run($route, $params=[])
 	{
-		if (($exception = Yii::$app->exception) === null) {
+		if (($exception = Yii::$app->errorHandler->exception) === null) {
 			return '';
 		}
 
@@ -53,7 +53,7 @@ class ErrorController extends Controller
 
 	public function actionIndex()
 	{
-		if (($exception = Yii::$app->exception) === null) {
+		if (($exception = Yii::$app->errorHandler->exception) === null) {
 			return '';
 		}
 
