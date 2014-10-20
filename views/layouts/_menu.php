450fec0fea995186b467d9dd2c4d6f58c935b9c2
Vitaliy Syrchikov <maddoger@gmail.com>
2014-10-20 14:24:03 +0400

    Активность меню. Потом надо доделать.

--- views/layouts/_menu.php
+++ views/layouts/_menu.php
@@ -5,6 +5,7 @@
  * @var string $content
  */
 use maddoger\admin\Module;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 
 /**
@@ -16,18 +17,45 @@ $menu = [
         'label' => Yii::t('maddoger/admin', 'Dashboard'),
         'class' => 'fa fa-dashboard',
         'url' => ['/'.Module::getInstance()->id.'/site/index'],
+        'sort' => -1,
     ],
 ];
 
+
+//Get navigation from modules
 foreach (Yii::$app->modules as $module) {
     if ($module instanceof \maddoger\core\BackendModule) {
 
+        $sort = $module->sortNumber;
         $navigation = $module->getNavigation();
+        foreach ($navigation as $key=>$value) {
+            if (!isset($navigation[$key]['sort'])) {
+                $navigation[$key]['sort'] = $sort;
+            }
+        }
         $menu = array_merge($menu, $navigation);
     }
 }
+//Сортируем меню
+usort($menu, function($a, $b){
+    $res = 0;
+    if ($a['sort'] != $b['sort']) {
+        $res = $a['sort']>$b['sort'] ? -1 : 1;
+    }
+    if (!$res) {
+        $res = strcmp($a['label'], $b['label']);
+    }
+    return $res;
+});
 
-function menu_items($items, $child=false)
+//Route and its params
+$currentRoute = null;
+$routeParams = Yii::$app->request->getQueryParams();
+if (Yii::$app->controller !== null) {
+    $currentRoute = Yii::$app->controller->getRoute();
+}
+
+$menu_items = function($items, $child=false) use (&$menu_items, $currentRoute)
 {
     if (!$items) {
         return '';
@@ -35,27 +63,51 @@ function menu_items($items, $child=false)
     $res = '';
     foreach ($items as $item) {
 
-        $content = Html::tag('span', $item['label']);
-        if (isset($item['icon'])) {
-            $content = Html::tag('i', '', ['class' => $item['icon']]).' ' . $content;
+        $label = ArrayHelper::getValue($item, 'label', '-undefined-');
+        $url = ArrayHelper::getValue($item, 'url', '#');
+        $icon = ArrayHelper::getValue($item, 'icon');
+        $items = ArrayHelper::getValue($item, 'items');
+        $options = ArrayHelper::getValue($item, 'options', []);
+        $active = ArrayHelper::getValue($item, 'active');
+        if ($active === null) {
+            $active = false;
+            if (is_array($url) && isset($url[0])) {
+                $route = $url[0];
+                if ($route[0] !== '/' && Yii::$app->controller) {
+                    $route = Yii::$app->controller->module->getUniqueId() . '/' . $route;
+                }
+                if (ltrim($route, '/') === $currentRoute) {
+                    $active = true;
+                }
+                //var_dump(ltrim($route, '/'), $currentRoute);
+            }
+        }
+        if ($active) {
+            Html::addCssClass($options, 'active');
         }
 
-        if (isset($item['items']) && !empty($item['items'])) {
-            $content .= Html::tag('i', '', ['class' => 'fa fa-angle-left pull-right']);
-            $content = Html::a($content, '#');
-            $content .= menu_items($item['items'], true);
-            $res .= Html::tag('li', $content, ['class' => 'treeview']);
-        } else {
-            $res .= Html::tag('li', Html::a($content, $item['url']));
+        $content = Html::tag('span', $label);
+        if ($icon) {
+            $content = Html::tag('i', '', ['class' => $icon]).' ' . $content;
         }
+
+        if ($items) {
+            $content .= Html::tag('i', '', ['class' => 'fa fa-angle-left pull-right']);
+            $content = Html::a($content, $url);
+            $content .= $menu_items($item['items'], true);
+            Html::addCssClass($options, 'treeview');
+        } else {
+            $content = Html::a($content, $url);
+        }
+        $res .= Html::tag('li', $content, $options);
 
     }
     if (!empty($res)) {
         return Html::tag('ul', $res, ['class' => $child ? 'treeview-menu' : 'sidebar-menu']);
     }
     return empty($res) ? '' : '<ul>'.$res.'</ul>';
-}
+};
 
-echo menu_items($menu);
+echo $menu_items($menu);
 
 ?>
