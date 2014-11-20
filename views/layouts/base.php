5420a679c088dadda8515c1cde353bcb2e627c1b
Vitaliy Syrchikov <maddoger@gmail.com>
2014-11-20 23:17:53 +0400

    Заголовок. Двойное экранирование.

--- views/layouts/base.php
+++ views/layouts/base.php
@@ -17,7 +17,7 @@ $bodyClass = isset($this->params['bodyClass']) ? $this->params['bodyClass'] : ''
     <meta charset="<?= Yii::$app->charset ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
+    <title><?= Html::encode($this->title, false) ?></title>
     <?php $this->head() ?>
 </head>
 <body class="<?= $bodyClass?>">
